4626b1f3cc3dccace64e1c62ae98fa508ccf5bd6
Simon Kirkham <Simon.Kirkham@kalibrate.com>
2019-03-28 14:17:55 +0000

    Initial projects

---
+++ CompositeWordsKataFast/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompositeWordsKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
