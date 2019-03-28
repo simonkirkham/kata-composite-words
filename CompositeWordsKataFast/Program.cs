1ddc99decfa6bcc5b178f44e3ef43176a4e3bd0e
Simon Kirkham <Simon.Kirkham@kalibrate.com>
2019-03-28 17:16:47 +0000

    Addign timings to fast solution

--- CompositeWordsKataFast/Program.cs
+++ CompositeWordsKataFast/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace CompositeWordsKataFast
 {
@@ -6,7 +7,12 @@ namespace CompositeWordsKataFast
     {
         static void Main(string[] args)
         {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
             Console.WriteLine("Hello World!");
+            stopwatch.Stop();
+            Console.WriteLine("Time Taken: " + stopwatch.Elapsed);
+            Console.ReadLine();
         }
     }
 }
